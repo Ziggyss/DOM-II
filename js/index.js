0730025c2600658c473479724b1472374619ac70
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-03 12:40:36 +0100

    add first three event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+const logoHeading = document.querySelector('.logo-heading');
+
+logoHeading.addEventListener('mouseover', () =>
+event.target.style.color = 'green');
+
+logoHeading.addEventListener('mouseleave', ()=> 
+event.target.style.color = 'black');
+
+const html = document.querySelector('html');
+
+html.addEventListener('keydown', () => 
+alert('Stop trying to type on this lovely webpage!'));
+
+
+
+
+
+
+
