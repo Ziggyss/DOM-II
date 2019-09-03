4d47976540abbb33abd8e8631ffe6a90605553c1
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-03 13:41:36 +0100

    add three more event listeners

--- js/index.js
+++ js/index.js
@@ -12,6 +12,30 @@ const html = document.querySelector('html');
 html.addEventListener('keydown', () => 
 alert('Stop trying to type on this lovely webpage!'));
 
+html.addEventListener('wheel', (event) => 
+alert('Wheeeee!!!'));
+
+const anchors = document.querySelectorAll('a');
+
+anchors.forEach(element => element.addEventListener('focus', (event) => {
+    event.target.style.background = 'teal';
+}));
+
+const image = document.querySelector('img');
+image.addEventListener('dblclick', (event) => {
+    event.target.style.display = 'none';
+})
+
+/* dblclick
+
+select
+
+resize
+
+select
+
+drag */
+
 
 
 
